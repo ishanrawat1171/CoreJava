f69bddb08b63b29ea2ea1203b1b1bd9da224588e
ishanrawat1171 <ishan.rawat@ltts.com>
2021-02-25 11:35:06 +0530

    Update FindId.java

--- prog/FindId.java
+++ prog/FindId.java
@@ -1,31 +1,41 @@
 
 import java.util.Scanner;
 public class FindId {
+	  static void myMethod(int n,int id[],int score[]) {
+		  int mark;
+		  Scanner sc=new Scanner(System.in);
+			
+			System.out.println("enter the score you want to set standard");
+			mark=sc.nextInt();
+			for(int i=0;i<n/2;i++) {
+				if(score[i]>mark) {
+					System.out.println(id[i]);
+				}
+			}
+	  
+			sc.close();
+		  }
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-int []id= new int[20];
-int []score=new int[20];
-int n,mark;
+
+int n;
 
 Scanner sc=new Scanner(System.in);
 System.out.println("enter number of elemets you want in array");
 n=sc.nextInt();
-
+int []id= new int[20];
+int []score=new int[20];
+Scanner sc1=new Scanner(System.in);
 for(int i=0;i<n/2;i++) {
-	System.out.println("enter id");
-	id[i]=sc.nextInt();
-	System.out.println("enter score");
-	score[i]=sc.nextInt();
+		System.out.println("enter id");
+		id[i]=sc1.nextInt();
+		System.out.println("enter score");
+		score[i]=sc1.nextInt();
 }
-System.out.println("enter the score you want to set standard");
-mark=sc.nextInt();
-for(int i=0;i<n/2;i++) {
-	if(score[i]>mark) {
-		System.out.println(id[i]);
-	}
-}
-sc.close();
+myMethod(n,id,score);
+sc1.close();
 	}
 	
 
